15f41111e0ed259128055beb8ca67ae633b3ce94
shannon-greenlight <shannon@greenlightdevelopment.com>
2023-12-07 13:29:42 -0800

    Renames .pde to .ino

--- Adafruit_LED_Backpack/examples/bicolor8x8/bicolor8x8.ino
+++ Adafruit_LED_Backpack/examples/bicolor8x8/bicolor8x8.ino
@@ -29,7 +29,7 @@ void setup() {
   Serial.begin(9600);
   Serial.println("8x8 LED Matrix Test");
   
-  matrix.begin(0x71);  // pass in the address
+  matrix.begin(0x70);  // pass in the address
 }
 
 static const uint8_t PROGMEM
